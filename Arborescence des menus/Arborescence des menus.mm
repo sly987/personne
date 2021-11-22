b0b7536cfc945fd3602d1c0f7bed5f1211d38d12
sly987 <76872198+sly987@users.noreply.github.com>
2021-11-22 16:42:47 +0100

    Add files via upload

---
+++ Arborescence des menus/Arborescence des menus.mm
@@ -0,0 +1,58 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1637590694463" ID="ID_68483305" MODIFIED="1637592263314" TEXT="password">
+<node CREATED="1637592270392" HGAP="26" ID="ID_1390086260" MODIFIED="1637592512213" POSITION="right" TEXT="c (g&#xe9;rer le compte root)" VSHIFT="-20">
+<node CREATED="1637592482431" HGAP="27" ID="ID_1122710708" MODIFIED="1637592641187" TEXT="l (afficher l&apos;employ&#xe9;)" VSHIFT="-24"/>
+<node CREATED="1637592642567" ID="ID_402904014" MODIFIED="1637592664147" TEXT="n (changer le nom)">
+<node CREATED="1637593631480" ID="ID_1691093018" MODIFIED="1637593640983" TEXT="saisir le nouveau nom"/>
+</node>
+<node CREATED="1637593514232" ID="ID_1941504510" MODIFIED="1637593527174" TEXT="p (changer le pr&#xe9;nom">
+<node CREATED="1637593661048" ID="ID_1334546687" MODIFIED="1637593669799" TEXT="saisir le nouveau pr&#xe9;nom"/>
+</node>
+<node CREATED="1637593531207" ID="ID_1944590225" MODIFIED="1637593542646" TEXT="e (changer le mail">
+<node CREATED="1637593685016" ID="ID_1021978986" MODIFIED="1637593689943" TEXT="saisir le nouveau mail"/>
+</node>
+<node CREATED="1637593549568" ID="ID_27645735" MODIFIED="1637593560838" TEXT="x (changer le password">
+<node CREATED="1637593701369" ID="ID_1498003718" MODIFIED="1637593720135" TEXT="saisir le nouveau password"/>
+</node>
+<node CREATED="1637593572680" ID="ID_1973535548" MODIFIED="1637593584822" TEXT="q (back)"/>
+</node>
+<node CREATED="1637592272216" ID="ID_1234264707" MODIFIED="1637592375121" POSITION="left" TEXT="q (quitter l&apos;application)"/>
+<node CREATED="1637592275336" ID="ID_1822787184" MODIFIED="1637592409482" POSITION="right" TEXT="l (g&#xe9;rer les ligues">
+<node CREATED="1637593752665" ID="ID_1540190870" MODIFIED="1637593773383" TEXT="l (afficher les ligues)"/>
+<node CREATED="1637593775905" ID="ID_1014875714" MODIFIED="1637593787463" TEXT="a (ajouter une ligue)">
+<node CREATED="1637593834233" ID="ID_1216774354" MODIFIED="1637593874104" TEXT="saisir le nom de la ligue"/>
+</node>
+<node CREATED="1637593790217" ID="ID_1643053646" MODIFIED="1637593801751" TEXT="e (s&#xe9;lectionner une ligue)">
+<node CREATED="1637593901938" ID="ID_1756715040" MODIFIED="1637593951814" TEXT="choisir la ligue &#xe0; &#xe9;diter ">
+<node CREATED="1637593955746" ID="ID_767392057" MODIFIED="1637593994210" TEXT="l (afficher la ligue)"/>
+<node CREATED="1637594003826" ID="ID_1008087844" MODIFIED="1637594669569" TEXT="e (G&#xe9;rer les employ&#xe9;s de la ligue s&#xe9;lectionn&#xe9;e)">
+<node CREATED="1637594118210" ID="ID_310853332" MODIFIED="1637594130648" TEXT="l (afficher les employ&#xe9;s)"/>
+<node CREATED="1637594135505" ID="ID_1949810633" MODIFIED="1637594142184" TEXT="a (ajouter un employ&#xe9;)">
+<node CREATED="1637594204130" ID="ID_426765708" MODIFIED="1637594327565" TEXT="saisir le nom / pr&#xe9;nom / mail / password de l&apos;employ&#xe9;"/>
+</node>
+<node CREATED="1637594148210" ID="ID_1226569656" MODIFIED="1637594156200" TEXT="e (modifier un employ&#xe9;)">
+<node CREATED="1637594344322" ID="ID_650729585" MODIFIED="1637594353289" TEXT="choix de l&apos;employ&#xe9;">
+<node CREATED="1637594370115" ID="ID_989684345" MODIFIED="1637594388088" TEXT="modifier le nom / pr&#xe9;nom / mail / password de l&apos;employ&#xe9;"/>
+</node>
+</node>
+<node CREATED="1637594162202" ID="ID_308921213" MODIFIED="1637594171000" TEXT="s (supprimer un employ&#xe9;)">
+<node CREATED="1637594804003" ID="ID_19513191" MODIFIED="1637594832073" TEXT="choisir l&apos;employ&#xe9; &#xe0; supprimer"/>
+</node>
+<node CREATED="1637594177970" ID="ID_1103386922" MODIFIED="1637594181832" TEXT="q (back)"/>
+</node>
+<node CREATED="1637594023706" ID="ID_900925313" MODIFIED="1637594037736" TEXT="c (changer d&apos;administrateur)">
+<node CREATED="1637595116955" ID="ID_1932879988" MODIFIED="1637595134825" TEXT="Choisir la ligue &#xe0; modifier"/>
+</node>
+<node CREATED="1637594040962" ID="ID_1428388541" MODIFIED="1637594056410" TEXT="r (renommer)">
+<node CREATED="1637595182962" ID="ID_1047471492" MODIFIED="1637595354505" TEXT="changer le nom de la ligue"/>
+</node>
+<node CREATED="1637594065114" ID="ID_369059498" MODIFIED="1637594071864" TEXT="d (supprimer)">
+<node CREATED="1637595403475" ID="ID_685998406" MODIFIED="1637595415944" TEXT="supprime la ligue choisie"/>
+</node>
+<node CREATED="1637594079746" ID="ID_257185086" MODIFIED="1637594085192" TEXT="q (back)"/>
+</node>
+</node>
+</node>
+</node>
+</map>
