42de7ad0b03d9daa7310ed4313c43d266c261472
sly987 <76872198+sly987@users.noreply.github.com>
2021-11-29 17:54:52 +0100

    test unitaire

--- Personnel/src/testsUnitaires/testLigue.java
+++ Personnel/src/testsUnitaires/testLigue.java
@@ -1,6 +1,7 @@
 package testsUnitaires;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 import personnel.*;
@@ -8,6 +9,7 @@ import personnel.*;
 class testLigue 
 {
 	GestionPersonnel gestionPersonnel = GestionPersonnel.getGestionPersonnel();
+
 	
 	@Test
 	void createLigue() throws SauvegardeImpossible
@@ -23,4 +25,29 @@ class testLigue
 		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty"); 
 		assertEquals(employe, ligue.getEmployes().first());
 	}
+	
+	@Test
+	void setNom() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		ligue.setNom("billard");
+		assertEquals("billard", ligue.getNom());
+	}
+	
+	@Test
+	void getAdmin() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty"); 
+		ligue.setAdministrateur(employe);
+		assertEquals(employe, ligue.getAdministrateur());
+	}
+	
+	void getAdminRoot() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe root = gestionPersonnel.getRoot();
+		assertThrows(DroitsInsuffisants.class, () -> ligue.setAdministrateur(root));
+	}
+	
 }
