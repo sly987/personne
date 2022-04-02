9c49389a4bafb302f98180edac5157949850ac03
Raphaelmrt <martin.raphael2201@gmail.com>
2022-04-02 17:58:47 +0200

    remettre l'option "changer admin

--- Personnel/src/commandLine/LigueConsole.java
+++ Personnel/src/commandLine/LigueConsole.java
@@ -71,7 +71,7 @@ public class LigueConsole
 		Menu menu = new Menu("Editer " + ligue.getNom());
 		menu.add(afficher(ligue));
 		menu.add(gererEmployes(ligue));
-		//menu.add(changerAdministrateur(ligue));
+		menu.add(changerAdministrateur(ligue));
 		menu.add(changerNom(ligue));
 		menu.add(supprimer(ligue));
 		menu.addBack("q");
@@ -125,7 +125,10 @@ public class LigueConsole
 	
 	private List<Employe> changerAdministrateur(final Ligue ligue)
 	{
-		return null;
+		return new List<>("Changer d'administrateur", "c", 
+				() -> new ArrayList<>(ligue.getEmployes()),
+				(index, element) -> {ligue.setAdministrateur(element);}
+				);
 	}		
 
 	private List<Employe> modifierEmploye(final Ligue ligue)
